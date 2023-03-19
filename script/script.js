4fd4cce52598b9795256cb2a1fc3f0557599366b
João Pedro Florenço <joaoflorenco@DESKTOP-QC7TF5U>
2023-03-19 19:44:49 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+function tableCalculation() {
+    let numberInput = document.getElementById('numberInput').value; 
+    let resultTable = document.getElementById('resultTable');
+
+    if(numberInput.length == 0) {
+        alert('[ERRO] IMPOSSÍVEL GERAR A TABUADA [ERRO]');
+    }
+    
+    numberInput = Number(numberInput);
+    
+    resultTable.innerHTML = '';
+    for (let i=1; i <= 10; i++) {
+        let optionElement = document.createElement('option');
+        optionElement.setAttribute('value', i);
+        optionElement.innerHTML = `${i} X ${numberInput} = ${numberInput*i}`;            
+        resultTable.appendChild(optionElement);
+    }
+    numberInput = document.getElementById('numberInput');
+    if(numberInput.value != '') {
+        numberInput.value = '';
+    }
+}
